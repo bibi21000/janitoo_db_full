9929e729f7df2abe54dbb75fbbeeb4e5c189f0bc
bibi21000 <bibi21000@gmail.com>
2016-04-09 14:55:15 +0200

    Update tests for docker

---
+++ tests/test_live.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""Unittests for models.
+"""
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015-2016 Sébastien GALLET aka bibi21000"
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys, os
+import time, datetime
+import unittest
+import threading
+import logging
+import inspect
+import glob
+from pkg_resources import iter_entry_points
+
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from janitoo_nosetests import JNTTBase
+#~ from janitoo_nosetests.models import JNTTModels
+
+from janitoo.options import JNTOptions
+from janitoo_db.base import Base, create_db_engine
+
+import janitoo_db.models as jntmodels
+
+#~ class TestModelsSQLite(JNTTModels, CommonModels):
+    #~ """Test the models
+    #~ """
+    #~ models_conf = "tests/data/janitoo_db.conf"
+#~
+#~ class TestModelsMySQL(JNTTModels, CommonModels):
+    #~ """Test the models
+    #~ """
+    #~ models_conf = "tests/data/janitoo_db_mysql.conf"
+#~
+#~ class TestModelsPostgresql(JNTTModels, CommonModels):
+    #~ """Test the models
+    #~ """
+    #~ models_conf = "tests/data/janitoo_db_postgres.conf"
+#~
+
+#~ print 'Named with wildcard:'
+#~ for name in glob.glob('tmp/tests/*/*.py'):
+    #~ print '\t', name
+    #~ print inspect.getmoduleinfo(name)
+    #~ classes = inspect.getmembers(name, inspect.isclass)
+    #~ print classes
+
